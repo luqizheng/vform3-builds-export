6a812b81bb138fbda2a1d93065d7ec56f408e3b2
leo.u <luqizheng@126.com>
2022-10-21 15:43:11 +0800

    change

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     terserOptions: {
       compress: {
         //生产环境时移除console
-        drop_console: true,
+        drop_console: false,
         drop_debugger: true,
       },
     },
